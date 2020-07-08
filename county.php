8ff3ec098767a455a34130510b5ee8279e33e0da
sapa2411 <steven.parsons.us11@gmail.com>
2020-07-08 01:51:12 -0400

    Dashboard by-county of NY state

---
+++ county.php
@@ -0,0 +1,58 @@
+<?php
+ //session_start();
+            //Establish connection with the database
+            $user = 'root';
+            $password = 'root';
+            $db = 'covid_us';
+            $host = 'localhost';
+            //$port = 8889;
+           
+
+            $conn = new mysqli($host, $user, $password, $db);
+
+            if (mysqli_connect_error()) {
+                echo "error connect";
+                die("Database connection failed: " . mysqli_connect_error());
+               
+            }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css"/>
+    <title>Document</title>
+</head>
+<body>
+<header>
+      <h1>BE ON TOP OF Covid-19</h1>
+      <h2>Novel Coronavirus Dashboard for the USA </h2>
+      <h5>updates every 30 minutes</h5>
+  </header>
+<span><h2>New York by County</h2></span>    
+</form>
+</fieldset>
+
+<?php
+$sql_total = mysqli_query($conn,"SELECT * FROM County");
+echo "<table class='tbstyle' >
+<tr>
+      <th>County</th>
+      <th>Cases</th>
+      <th>Deaths</th>
+</tr>
+  ";
+while($row = $sql_total ->fetch_assoc()){
+    echo "<tr>";
+        echo "<td>" . $row['county_name']."</td>";
+        echo "<td>" . $row['cases']."</td>";
+        echo "<td>" . $row['deaths']."</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+?>
+
+</body>
+</html>
